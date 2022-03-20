d0d271be1f955ae0f1a35e30bb8e54c79bd3eae1
noviarrizal <noviarrizalarif@gmail.com>
2022-03-20 13:44:25 +0800

    finish UI e-commerse website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,16 @@ import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 function App() {
 	return (
-		// <Home />
+		<Home />
 		// <ProductList />
 		// <Product />
 		// <Register />
-		<Login />
+		// <Login />
+		// <Cart />
 	);
 }
 
