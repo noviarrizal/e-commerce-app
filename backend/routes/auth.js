9c79c7db36f3ae79f74d39f4c66d830a8d2bad16
noviarrizal <noviarrizalarif@gmail.com>
2022-03-20 16:15:49 +0800

    Adding backend and connect to mongoDB

---
+++ backend/routes/auth.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+
+// REGISTER
+router.post("/register", async (req, res) => {
+	const newUser = new User({
+		username: req.body.username,
+		email: req.body.email,
+		password: CryptoJS.AES.encrypt(
+			req.body.password,
+			process.env.PASS_SEC
+		).toString(),
+	});
+	try {
+		const savedUser = await newUser.save();
+		res.status(201).json(savedUser);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+	try {
+		const user = await User.findOne({ username: req.body.username });
+		!user && res.status(401).json("Wrong Credentials!");
+		const hashedPassword = CryptoJS.AES.decrypt(
+			user.password,
+			process.env.PASS_SEC
+		);
+		const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+		password !== req.body.password &&
+			res.status(401).json("Wrong Credentials!");
+		res.status(200).json(user);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+module.exports = router;
